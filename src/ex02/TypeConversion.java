38434befaa5f547262a849f0e5d67943c81cb966
thdus <thdus1323@naver.com>
2023-12-13 11:16:08 +0900

    캐ㅐì스팅과 상수

---
+++ src/ex02/TypeConversion.java
@@ -0,0 +1,21 @@
+package ex02;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        int;
+        double f;
+
+        f = 1 / 5;
+        System.out.println(f);
+
+        f = (double) 1 / 5;
+        System.out.println(f);
+
+        i = (int) 1.7 + (int) 1.8;
+        System.out.println(i);
+   }
+}
+
+
+
+
