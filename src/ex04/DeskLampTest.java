c5ac70c0d32b38d7eaef03320e4d75166ca540ea
thdus <thdus1323@naver.com>
2023-12-20 11:08:35 +0900

    클래스 기본 문법

---
+++ src/ex04/DeskLampTest.java
@@ -0,0 +1,9 @@
+package ex04;
+
+public class DeskLampTest {
+    private boolean isOn;
+
+    public void turnOn() { isOn = true; }
+    public void turnOff() { isOn = false;}
+    public String toS
+}
