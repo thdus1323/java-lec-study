5afb533dd8d3b478b0ba7a60657086e5fdce4904
thdus <thdus1323@naver.com>
2023-12-22 11:36:02 +0900

    재정의, 다형성, 오버라이드, 동적바인딩

---
+++ src/ex06/CircleTest.java
@@ -0,0 +1,33 @@
+package ex06;
+
+class Shape{
+    int x,y;
+
+    public Shape(){
+        System.out.println("Shape 생성됨");
+    }
+}
+
+class Circle extends Shape{
+    int radius; // 반지름
+
+    public Circle(int radius) {
+        System.out.println("Circle 생성됨");
+        this.radius = radius;
+        System.out.println("1");
+        super.x = 1;
+        super.y = 2;
+    }
+
+    double getArea(){
+        return 3.14 * radius * radius;
+    }
+}
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        circle.getArea();
+
+    }
+}
